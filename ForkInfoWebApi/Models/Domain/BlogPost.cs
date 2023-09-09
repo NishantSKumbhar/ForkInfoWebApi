7b61c3d63b83d09ecda774ee8434ea2287e51fde
Nishant Sanjay Kumbhar <nishantsk210@gmail.com>
2023-09-09 22:26:32 +0530

    sam

--- ForkInfoWebApi/Models/Domain/BlogPost.cs
+++ ForkInfoWebApi/Models/Domain/BlogPost.cs
@@ -2,6 +2,7 @@
 {
     public class BlogPost
     {
+        //
         public Guid Id { get; set; }
         public string Title { get; set; }
         public string  ShortDescription { get; set; }
